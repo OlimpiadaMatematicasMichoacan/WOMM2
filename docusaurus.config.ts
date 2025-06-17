1f49fd29b312055551f9598fa51ee3953372e730
zǝʇɹoɔ oɓıɹpoɹ <28023409+roicort@users.noreply.github.com>
2025-06-17 15:26:30 -0600

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,15 +10,15 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://michoacan.olimpiadamatematicas.org',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'OlimpiadaMatematicasMichoacan', // Usually your GitHub org/user name.
+  projectName: 'WOMM2', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -40,7 +40,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/OlimpiadaMatematicasMichoacan/WOMM2/',
         },
         blog: {
           showReadingTime: true,
@@ -72,10 +72,10 @@ const config: Config = {
       },
       items: [
         { to: '/acerca', label: 'Acerca', position: 'right' },
-        { to: '/blog/tags/resultados', label: 'Resultados', position: 'right' },
-        { to: '/blog/tags/convocatorias', label: 'Convocatoria', position: 'right' },
-        { to: '/blog/tags/noticias', label: 'Noticias', position: 'right' },
         { to: '/blog/authors', label: 'Equipo', position: 'right' },
+        { to: '/blog/tags/noticias', label: 'Noticias', position: 'right' },
+        { to: '/blog/tags/convocatorias', label: 'Convocatoria', position: 'right' },
+        { to: '/blog/tags/resultados', label: 'Resultados', position: 'right' },
         {
           type: 'docSidebar',
           sidebarId: 'tutorialSidebar',
@@ -102,7 +102,7 @@ const config: Config = {
     announcementBar: {
       id: 'support_us',
       content:
-        'Este sitio web es un trabajo en progreso. Si quiere ayudar considere contribuir en Github.',
+        'Este es el nuevo sitio web de la OMM. Por favor háganos llegar sus comentarios en Github.',
       isCloseable: true,
     },
     footer: {
@@ -121,16 +121,12 @@ const config: Config = {
           title: 'Redes Sociales',
           items: [
             {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/omm',
+              label: 'WhatsApp',
+              href: 'https://chat.whatsapp.com/EusFmjYySlF2E8tmZPZHss',
             },
             {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/omm',
-            },
-            {
-              label: 'X',
-              href: 'https://x.com/omm',
+              label: 'Facebook',
+              href: 'https://www.facebook.com/olimpiadamichoacanadematematicas',
             },
           ],
         },
@@ -148,7 +144,7 @@ const config: Config = {
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} OMM. </br> Hecho en México por <a href="https://roicort.github.io">Rodrigo Cortez</a> con OSS y ❤️.`,
+      copyright: `Copyright © ${new Date().getFullYear()} OMM. </br> Hecho en Morelia por <a href="https://roicort.github.io">Rodrigo Cortez</a> con <a href="https://docusaurus.io/">OSS</a> y ❤️.`,
     },
     prism: {
       theme: prismThemes.github,
